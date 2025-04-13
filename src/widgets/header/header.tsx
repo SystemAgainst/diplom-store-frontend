f5623f2a3af570e4b0bd837cf522ca7d61492a65
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2025-04-13 23:46:26 +1000

    feat: remove useless code from header

--- src/widgets/header/header.tsx
+++ src/widgets/header/header.tsx
@@ -1,14 +1,9 @@
-import { Link, useLocation } from "react-router-dom";
 import styles from "./header.module.css";
 import { useUserStore } from "@/features/user/model/user-store.ts";
 import { authApi } from "@/features/auth/model/api.ts";
 
-/*
-* TODO: fix user?.login -> show data (not null)
-* */
 
 export const Header = () => {
-  const location = useLocation();
   const user = useUserStore((s) => s.user);
   const setUser = useUserStore((s) => s.setUser);
 
@@ -17,25 +12,10 @@ export const Header = () => {
     setUser(null);
   };
 
-  const isActive = (path: string) => location.pathname === path;
   return (
     <header className={styles.header}>
       <div className={styles.headerContent}>
-        <h1 className={styles.headerTitle}>Anti-patterns</h1>
-        <nav className={styles.headerNav}>
-          <Link
-            to="/#"
-            className={`${styles.navLink} ${isActive("/tracks") ? styles.active : ""}`}
-          >
-            About
-          </Link>
-          <Link
-              to="/#"
-              className={`${styles.navLink} ${isActive("/tasks") ? styles.active : ""}`}
-          >
-            More
-          </Link>
-        </nav>
+        <h1 className={styles.headerTitle}>DiplomStore</h1>
         <div className={styles.headerUser}>
           <span className={styles.username}>{user?.login}</span>
           <button className={styles.logoutButton} onClick={handleLogout}>
