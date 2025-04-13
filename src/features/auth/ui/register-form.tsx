25a8c77a186e1ad530e2ab068f70c64f625364e1
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2025-04-13 11:53:32 +1000

    feat: upd layout for register form

--- src/features/auth/ui/register-form.tsx
+++ src/features/auth/ui/register-form.tsx
@@ -1,85 +1,143 @@
 import { useState } from "react";
 import styles from "./style.module.css";
-import {Link} from "react-router-dom";
-import {routes} from "@/shared/config/routes.ts";
-// import { useAuth } from "../model/auth";
+import { Link } from "react-router-dom";
+import { routes } from "@/shared/config/routes";
+import { Role, ROLES } from "@/features/auth/model/types";
+import { authApi } from "../model/api";
+import {useUserStore} from "@/features/user/model/user-store.ts";
+
 
 interface RegisterFormData {
-  username: string;
-  email: string;
+  login: string;
+  loginTelegram: string;
   password: string;
+  chatId: string;
+  role: Role;
 }
 
 export const RegisterForm = () => {
+  const setUser = useUserStore((s) => s.setUser);
+
   const [formData, setFormData] = useState<RegisterFormData>({
-    email: "",
+    loginTelegram: "",
     password: "",
-    username: "",
+    login: "",
+    chatId: "",
+    role: ROLES.SUPPLIER,
   });
   const [error, setError] = useState<string>("");
 
-  // const { register } = useAuth();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      // await register(formData);
+      const user = await authApi.register(formData);
+      setUser(user);
     } catch (err) {
       setError(err instanceof Error ? err.message : "Registration failed");
     }
   };
 
   return (
-    <form onSubmit={handleSubmit} className={styles.formContainer}>
-      <div>
-        <label htmlFor="username" className={styles.label}>
-          Username
-        </label>
-        <input
-          type="text"
-          id="username"
-          value={formData.username}
-          onChange={(e) =>
-            setFormData({ ...formData, username: e.target.value })
-          }
-          className={styles.inputGroup}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="email" className={styles.label}>
-          Email
-        </label>
-        <input
-          type="email"
-          id="email"
-          value={formData.email}
-          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-          className={styles.inputGroup}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="password" className={styles.label}>
-          Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          value={formData.password}
-          onChange={(e) =>
-            setFormData({ ...formData, password: e.target.value })
-          }
-          className={styles.inputGroup}
-          required
-        />
-      </div>
-      {error && <div className={styles.error}>{error}</div>}
-      <button type="submit" className={styles.button}>
-        Зарегистрироваться
-      </button>
+      <form onSubmit={handleSubmit} className={styles.formContainer}>
+        <div>
+          <label htmlFor="login" className={styles.label}>
+            Username
+          </label>
+          <input
+              type="text"
+              id="login"
+              value={formData.login}
+              onChange={(e) =>
+                  setFormData({...formData, login: e.target.value})
+              }
+              className={styles.inputGroup}
+              required
+          />
+        </div>
 
-      <span>У вас уже есть аккаунта? <Link to={routes.login}>Войдите</Link> в систему</span>
-    </form>
+        <div>
+          <label htmlFor="loginTelegram" className={styles.label}>
+            Telegram Login
+          </label>
+          <input
+              type="text"
+              id="loginTelegram"
+              value={formData.loginTelegram}
+              onChange={(e) =>
+                  setFormData({...formData, loginTelegram: e.target.value})
+              }
+              className={styles.inputGroup}
+              required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="chatId" className={styles.label}>
+            Chat ID
+          </label>
+          <input
+              type="text"
+              id="chatId"
+              value={formData.chatId}
+              onChange={(e) =>
+                  setFormData({...formData, chatId: e.target.value})
+              }
+              className={styles.inputGroup}
+              required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password" className={styles.label}>
+            Password
+          </label>
+          <input
+              type="password"
+              id="password"
+              value={formData.password}
+              onChange={(e) =>
+                  setFormData({...formData, password: e.target.value})
+              }
+              className={styles.inputGroup}
+              required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="role" className={styles.label}>
+            Роль
+          </label>
+          <select
+              id="role"
+              value={formData.role || ""}
+              onChange={(e) =>
+                  setFormData({...formData, role: e.target.value as Role})
+              }
+              className={styles.inputGroup}
+              required
+          >
+            <option value="" disabled>
+              -- Выберите роль --
+            </option>
+            {Object.entries(ROLES).map(([key, value]) => (
+                <option key={key} value={value}>
+                  {value}
+                </option>
+            ))}
+          </select>
+        </div>
+
+        {error && <div className={styles.error}>{error}</div>}
+
+        <button type="submit" className={styles.button}>
+          Зарегистрироваться
+        </button>
+
+        <span>
+      У вас уже есть аккаунт?{" "}
+          <Link to={routes.login}>Войдите</Link> в систему
+    </span>
+      </form>
   );
 };
